69eab1ed892527dbf46499146fcdec204ecf5c8f
Pbailey14 <peter04bailey@gmail.com>
2025-03-15 16:52:57 -0400

    Fixed player movement moving only player one with both wasd and arrow keys

--- SoccerMomsVan/Assets/Scripts/PlayerController.cs
+++ SoccerMomsVan/Assets/Scripts/PlayerController.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
-    public bool isPlayerOne; // Toggle for Player1 or Player2
+    public bool isPlayerOne; // True = WASD, False = Arrow Keys
     private Rigidbody2D rb;
+    private Vector2 movement;
 
     void Start()
     {
@@ -13,15 +14,26 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        float move = 0;
-        if (isPlayerOne)
+        movement = Vector2.zero; // Reset movement each frame
+
+        if (isPlayerOne) // Player 1 uses WASD
         {
-            move = Input.GetAxisRaw("Vertical"); // Uses W and S
+            if (Input.GetKey(KeyCode.W)) movement.y = 1;
+            if (Input.GetKey(KeyCode.S)) movement.y = -1;
+            if (Input.GetKey(KeyCode.A)) movement.x = -1;
+            if (Input.GetKey(KeyCode.D)) movement.x = 1;
         }
-        else
+        else // Player 2 uses Arrow Keys
         {
-            move = Input.GetAxisRaw("Vertical2"); // Uses Up and Down Arrow keys
+            if (Input.GetKey(KeyCode.UpArrow)) movement.y = 1;
+            if (Input.GetKey(KeyCode.DownArrow)) movement.y = -1;
+            if (Input.GetKey(KeyCode.LeftArrow)) movement.x = -1;
+            if (Input.GetKey(KeyCode.RightArrow)) movement.x = 1;
         }
-        rb.linearVelocity = new Vector2(0, move * speed);
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = movement.normalized * speed; // Normalize to avoid faster diagonal movement
     }
 }
